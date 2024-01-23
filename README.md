e2a911dc72ab30344ac7abfd08e63b68906f78be
berrymot <merrybot47@gmail.com>
2024-01-22 20:30:20 -0800

    make and document structs

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 # notoize
 
-a crate that tells you what Noto font stack you need
+A crate that tells you what Noto font stack you need.
+
+## Per-font options (preferences)
+
+
+```rust
+let config = NotoizeConfig {
+    prefer_math: true,
+    adlam: vec![AdlamNkoCfg::Unjoined],
+    ..NotoizeConfig::new_sans()
+};
+```
+
+### bools
+
+- `prefer_math`: over Noto Sans Symbols, for the 222 characters in both
+
+### Script-specific
+
+You supply lists to `NotoizeConfig` for `notoize()`.
+
+There are no options for
+- Music
+- **Sans only:** Anatolian Hieroglyphs, Avestan, Bamum, Bassa Vah, Batak, Bhaisuki, Brahmi, Buginese, Buhid, Canadian Aboriginal, Carian, Caucasian Albanian, Chakma, Cham, Cherokee, Chorasmian, Coptic, Cuneiform, Cypriot, Cypro-Minoan, Deseret, Duployan, Egyptian Hieroglyphs, Elbasan, Elymaic, Glagolitic, Gothic, Gunjala Gondi, Hanifi Rohingya, Hanunoo, Hatran, Imperial Aramaic, Indic Siyaq Numbers, Inscriptional Pahlavi, Inscriptional Parthian, Javanese, Kaithi, Kawi, Kayah Li, Kharoshthi, Khudawadi, Lepcha, Limbu, Linear A, Linear B, Lisu, Lycian, Lydian, Mahajani, Mandaic, Manichaean, Masaram Gondi, Math, Mayan Numerals, Medefaidrin, Meetei Mayek, Mende Kikakui, Meroitic, Miao, Modi, Mongolian, Mro, Multani, Nabataean, Nag Mundari, Nandinagari, Newa, New Tai Lue, Ogham, Ol Chiki, Old Hungarian, Old Italic, Old North Arabian, Old Permic, Old Persian, Old Sogdian, Old South Arabian, Old Turkic, Osage, Osmanya, Pahawh Hmong, Palmyrene, Pau Cin Hau, Phags Pa, Phoenician, Psalter Pahlavi, Rejang, Samaritan, Saurashtra, Sharada, Shavian, Siddham, SignWriting, Sogdian, Sora Sopeng, Soyombo, Sunandese, Syloti Nagri, Symbols, Symbols 2, Tagalog, Tagbanwa, Tai Le, Tai Tham, Tai Viet, Takri, Tamil Supplement, Tangsa, Thaana, Tifinagh, Tirhuta, Ugaritic, Vai, Wancho, Warang Citi, Yi, Zananbazar Square
+- **Serif only**: Ahom, Dives Akuru, Dogra, Makasar, Nyiakeng Puachue Hmong, Old Uyghur, Ottoman Siyaq, Tibetan, Toto, Yezidi
+
+Noto Sans Mono and Noto Sans/Serif Display are not supplied.
+
+| script | default sans | default serif | other |
+|:-|:-|:-|:-|
+| LGC ('regular') | `Sans` | `Serif` | - |
+| Adlam, N'ko | `Sans` | - | `Unjoined` |
+| Arabic | `Sans` | `Naskh` | `Kufi`, `NaskhUi`, `Nastaliq` |
+| Hebrew | `Sans` | `Serif` | `Rashi` |
+| Khitan | - | `Serif` | `Rotated`, `Vertical` |
+| Nushu | `Sans` | - | `Traditional` |
+| Syriac | `Sans` | - | `Eastern`, `Western` |
+| Thai, Lao | `SansUnlooped` | `Serif` | `SansLooped` |
+
+Everything else (Armenian, Balinese, Bengali, Devanagari, Ethiopic, Georgian, Grantha, Gujarati, Gurmukhi, Kannada, Khmer, Khojki, Malayalam, Myanmar, Oriya, Sinhala, Tamil, Telugu, Vithkuqi) is only either `Sans` or `Serif`, accessible via the `Serifness` enum.
+
+There are all-sans and all*-serif presets as `new_sans()` / `prefer_serif()`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,118 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// use serde_json;
+
+pub struct NotoizeConfig {
+    pub lgc: Vec<Serifness>,
+    pub armenian: Vec<Serifness>,
+    pub balinese: Vec<Serifness>,
+    pub bengali: Vec<Serifness>,
+    pub devanagari: Vec<Serifness>,
+    pub ethiopic: Vec<Serifness>,
+    pub georgian: Vec<Serifness>,
+    pub grantha: Vec<Serifness>,
+    pub gujarati: Vec<Serifness>,
+    pub gurmukhi: Vec<Serifness>,
+    pub kannada: Vec<Serifness>,
+    pub khmer: Vec<Serifness>,
+    pub khojki: Vec<Serifness>,
+    pub malayalam: Vec<Serifness>,
+    pub myanmar: Vec<Serifness>,
+    pub oriya: Vec<Serifness>,
+    pub sinhala: Vec<Serifness>,
+    pub tamil: Vec<Serifness>,
+    pub telugu: Vec<Serifness>,
+    pub vithkuqi: Vec<Serifness>,
+    // not just sans/serif
+    pub adlam: Vec<AdlamNkoCfg>,
+    pub nko: Vec<AdlamNkoCfg>,
+    pub arabic: Vec<ArabicCfg>,
+    pub hebrew: Vec<HebrewCfg>,
+    pub khitan: Vec<KhitanCfg>,
+    pub nushu: Vec<NushuCfg>,
+    pub syriac: Vec<SyriacCfg>,
+    pub thai: Vec<ThaiLaoCfg>,
+    pub lao: Vec<ThaiLaoCfg>,
+}
+impl NotoizeConfig {
+    pub fn new_sans() -> Self {
+        Self {
+            lgc: vec![Serifness::Sans],
+            armenian: vec![Serifness::Sans],
+            balinese: vec![Serifness::Sans],
+            bengali: vec![Serifness::Sans],
+            devanagari: vec![Serifness::Sans],
+            ethiopic: vec![Serifness::Sans],
+            georgian: vec![Serifness::Sans],
+            grantha: vec![Serifness::Sans],
+            gujarati: vec![Serifness::Sans],
+            gurmukhi: vec![Serifness::Sans],
+            kannada: vec![Serifness::Sans],
+            khmer: vec![Serifness::Sans],
+            khojki: vec![Serifness::Sans],
+            malayalam: vec![Serifness::Sans],
+            myanmar: vec![Serifness::Sans],
+            oriya: vec![Serifness::Sans],
+            sinhala: vec![Serifness::Sans],
+            tamil: vec![Serifness::Sans],
+            telugu: vec![Serifness::Sans],
+            vithkuqi: vec![Serifness::Sans],
+            //
+            adlam: vec![AdlamNkoCfg::Sans],
+            nko: vec![AdlamNkoCfg::Sans],
+            arabic: vec![ArabicCfg::Sans],
+            hebrew: vec![HebrewCfg::Sans],
+            khitan: vec![KhitanCfg::Serif],
+            nushu: vec![NushuCfg::Sans],
+            syriac: vec![SyriacCfg::Sans],
+            thai: vec![ThaiLaoCfg::SansUnlooped],
+            lao: vec![ThaiLaoCfg::SansLooped],
+        }
+    }
+    pub fn prefer_serif() -> Self {
+        Self {
+            lgc: vec![Serifness::Serif],
+            armenian: vec![Serifness::Serif],
+            balinese: vec![Serifness::Serif],
+            bengali: vec![Serifness::Serif],
+            devanagari: vec![Serifness::Serif],
+            ethiopic: vec![Serifness::Serif],
+            georgian: vec![Serifness::Serif],
+            grantha: vec![Serifness::Serif],
+            gujarati: vec![Serifness::Serif],
+            gurmukhi: vec![Serifness::Serif],
+            kannada: vec![Serifness::Serif],
+            khmer: vec![Serifness::Serif],
+            khojki: vec![Serifness::Serif],
+            malayalam: vec![Serifness::Serif],
+            myanmar: vec![Serifness::Serif],
+            oriya: vec![Serifness::Serif],
+            sinhala: vec![Serifness::Serif],
+            tamil: vec![Serifness::Serif],
+            telugu: vec![Serifness::Serif],
+            vithkuqi: vec![Serifness::Serif],
+            //
+            arabic: vec![ArabicCfg::Naskh],
+            hebrew: vec![HebrewCfg::Serif],
+            khitan: vec![KhitanCfg::Serif],
+            thai: vec![ThaiLaoCfg::Serif],
+            lao: vec![ThaiLaoCfg::Serif],
+            ..Self::new_sans()
+        }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub enum Serifness {Sans, Serif}
+pub enum AdlamNkoCfg {Sans, Unjoined}
+pub enum ArabicCfg {Sans, Kufi, Naskh, NaskhUi, Nastaliq}
+pub enum HebrewCfg {Sans, Serif, Rashi}
+pub enum KhitanCfg {Serif, Vertical, Rotated}
+pub enum NushuCfg {Sans, Traditional}
+pub enum SyriacCfg {Sans, Western, Eastern}
+pub enum ThaiLaoCfg {SansLooped, SansUnlooped, Serif}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct FontStack(Vec<String>);
+
+/// Returns a minimal CSS font stack for rendering `text`
+pub fn notoize(text: &str, config: NotoizeConfig) -> FontStack {
+    drop(config);
+    FontStack(vec![text.to_string()])
 }
