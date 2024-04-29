042130c4b7c9c99d22079bc5d3a5ba42984a27bc
berrymot <merrybot47@gmail.com>
2024-04-29 15:48:17 -0700

    remove `phags-pa`, don't uselessly write things

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 use notoize::*;
-use std::{fs, time::Instant};
+use std::time::Instant;
 
 fn main() {
     let start = Instant::now();
     let mut client = NotoizeClient::new();
-    let the = client
+    let _ = client
         .notoize(&(0..0x110000).filter_map(char::from_u32).collect::<String>())
         .files();
-    fs::remove_dir_all("test").unwrap_or_default();
-    fs::create_dir("test").unwrap();
-    for f in the {
-        fs::write(format!("test/{}", f.filename), &f.bytes).unwrap();
-    }
     println!("{:?}", start.elapsed());
 }
