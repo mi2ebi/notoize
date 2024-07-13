8ba76c8676138e877c68299db3549ea5cd491dfb
berrymot <merrybot47@gmail.com>
2024-07-12 20:16:06 -0700

    output the fonts

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use std::{fs, time::Instant};
 fn main() {
     let start = Instant::now();
     let mut client = NotoizeClient::new();
-    let the = client
-        .notoize(&(0..0x110000).filter_map(char::from_u32).collect::<String>())
-        .map_string();
-    fs::write(".notoize/mapping.txt", the.all).unwrap();
-    fs::write(".notoize/script_conflicts.txt", the.conflicts).unwrap();
-    fs::write(".notoize/missing_variants.txt", the.missing).unwrap();
+    let the = client.notoize(&(0..0x110000).filter_map(char::from_u32).collect::<String>());
+    let map = the.clone().map_string();
+    fs::write("out/data/mapping.txt", map.all).unwrap();
+    fs::write("out/data/script_conflicts.txt", map.conflicts).unwrap();
+    fs::write("out/data/missing_variants.txt", map.missing).unwrap();
+    for font in the.files() {
+        fs::write(format!("out/fonts/{}", font.filename), font.bytes).unwrap();
+    }
     println!("{:?}", start.elapsed());
 }
